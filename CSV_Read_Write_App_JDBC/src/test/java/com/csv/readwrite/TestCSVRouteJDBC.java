faf42d855ac70b856d1a5bd668c6dd48ed14f5a3
gauravdhamal <gauravdhamal11@gmail.com>
2023-05-25 12:35:56 +0530

    Unit testing for JDBC app.

---
+++ CSV_Read_Write_App_JDBC/src/test/java/com/csv/readwrite/TestCSVRouteJDBC.java
@@ -0,0 +1,26 @@
+package com.csv.readwrite;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+public class TestCSVRouteJDBC extends CamelTestSupport {
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:src/main/resources?fileName=empData.csv&noop=true").to("mock:result");
+            }
+        };
+    }
+
+    @Test
+    public void testJDBCRoute() throws InterruptedException {
+        getMockEndpoint("mock:result").expectedMessageCount(1);
+
+        MockEndpoint.assertIsSatisfied(context);
+    }
+}
